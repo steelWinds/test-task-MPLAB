f5384d43e978d93fdd7536028e976eb7fc00effa
Kirill <kirillsurov0@gmail.com>
2023-03-09 21:59:29 +0900

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -9,36 +9,3 @@ let palindromeNew = str => str.replaceAll(anyWord, '').toLowerCase() === str.rep
 if (palindrome('Lewd did I live, & evil I did dwel')) {
   console.log('YOLO')
 }
-
-// Task 2: OLD
-var multiplyAllOld = a => b => a.map(w = w*b)
-
-// Task 2: NEW
-var multiplyAllNew = a => b => a.map(n => n * b)
-
-console.log(multiplyAllNew([1, 2, 3, 4])(2))
-
-// Task 3: OLD
-function wordsToMarksOld(string){
-  var res = []
-  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  for (s of string){
-    res.push(alphabet.findIndex(i => i == s) + 1)
-  }
-
-  return res.reduce((partialSum, a) => { return partialSum + a}, 0)
-}
-
-// Task 3: NEW
-function wordsToMarksNew(string){
-  let res = 0
-  let alpha = [...Array.from({length: 121 - 96}, (_, v) => String.fromCharCode(v + 97))]
-
-  Array.from(string).forEach(s => res += alpha.findIndex(i => i === s) + 1)
-
-  return res
-}
-
-if (wordsToMarksOld('abcasdfasdf') === wordsToMarksNew('abcasdfasdf')) {
-  console.log('YOLO')
-}
